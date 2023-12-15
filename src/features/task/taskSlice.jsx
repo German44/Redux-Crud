a616dae2af2cae3f483475401c00fcd7c1227687
German Ravarotto <g44ravarotto@gmail.com>
2023-12-15 09:37:08 -0300

    finish

--- src/features/task/taskSlice.jsx
+++ src/features/task/taskSlice.jsx
@@ -26,7 +26,12 @@ export const taskSlice = createSlice({
             return state.filter(task => task.id !== action.payload);
         },
         editTask: (state, action) => {
-
+            const { id, title, description } = action.payload;
+            const task = state.find(task => task.id === id);
+            if (task) {
+                task.title = title;
+                task.description = description;
+            }
         }
     },
 })
